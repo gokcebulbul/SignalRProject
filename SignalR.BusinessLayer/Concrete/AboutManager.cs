69def0e39052ce96e9510f436de9be412947113b
gokcebulbul <146324263+gokcebulbul@users.noreply.github.com>
2024-04-23 21:24:14 +0300

    Add project files.

---
+++ SignalR.BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,41 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDal _abutDal;
+
+        public AboutManager(IAboutDal abutDal)
+        {
+            _abutDal = abutDal;
+        }
+
+        public void TAdd(About entity)
+        {
+            _abutDal.Add(entity);
+        }
+
+        public void TDelete(About entity)
+        {
+            _abutDal.Delete(entity);
+        }
+
+        public About TGetByID(int id)
+        {
+            return _abutDal.GetByID(id);
+        }
+
+        public List<About> TGetListAll()
+        {
+           return _abutDal.GetListAll();
+        }
+
+        public void TUpdate(About entity)
+        {
+            _abutDal.Update(entity);
+        }
+    }
+}
---
+++ SignalRWebUI/ViewComponents/Layout Components/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.Layout_Components
+{
+	public class _LayoutSidebarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}

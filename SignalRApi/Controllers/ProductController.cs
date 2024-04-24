0df5ad10464f707333ca29f50fc90b69721f75b3
gokcebulbul <146324263+gokcebulbul@users.noreply.github.com>
2024-04-24 19:45:59 +0300

    Ürün işlemleri tamamlandı

--- SignalRApi/Controllers/ProductController.cs
+++ SignalRApi/Controllers/ProductController.cs
@@ -55,7 +55,8 @@ namespace SignalRApi.Controllers
                ImageUrl = createProductDto.ImageUrl,
                Price = createProductDto.Price,
                ProductName = createProductDto.ProductName,
-               ProductStatus = createProductDto.ProductStatus
+               ProductStatus = createProductDto.ProductStatus,
+               CategoryID = createProductDto.CategoryID,
             });
             return Ok("Ürün Bilgisi Eklendi");
         }
@@ -66,7 +67,7 @@ namespace SignalRApi.Controllers
             _productService.TDelete(value);
             return Ok("Ürün Bilgisi Silindi");
         }
-        [HttpGet("GetProduct")]
+        [HttpGet("{id}")]
         public IActionResult GetProduct(int id)
         {
             var value = _productService.TGetByID(id);
@@ -82,7 +83,8 @@ namespace SignalRApi.Controllers
                 ImageUrl = updateProductDto.ImageUrl,
                 Price = updateProductDto.Price,
                 ProductName = updateProductDto.ProductName,
-                ProductStatus = updateProductDto.ProductStatus
+                ProductStatus = updateProductDto.ProductStatus,
+                CategoryID = updateProductDto.CategoryID,
 
             });
             return Ok("Ürün Bilgisi Güncellendi");

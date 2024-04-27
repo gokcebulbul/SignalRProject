e19e7f168e73f76c6eaf205b45cec14a989620fd
gokcebulbul <146324263+gokcebulbul@users.noreply.github.com>
2024-04-27 23:31:03 +0300

    Rezervasyon ve iletişim sayfası eklendi

---
+++ SignalRWebUI/Dtos/AboutDtoss/CreateAboutDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRWebUI.Dtos.AboutDtoss
+{
+    public class CreateAboutDto
+    {
+        public string ImageUrl { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}

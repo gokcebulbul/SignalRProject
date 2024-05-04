ecfcd053ffee5f4adb7209edd45c76e79befd846
gokcebulbul <146324263+gokcebulbul@users.noreply.github.com>
2024-05-04 17:38:21 +0300

    Sosyal medya ve referas sayfaları eklendi

---
+++ SignalRWebUI/Dtos/SocialMediaDtos/ResultSocialMediaDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalRWebUI.Dtos.SocialMediaDtos
+{
+    public class ResultSocialMediaDto
+    {
+        public int SocialMediaID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Icon { get; set; }
+    }
+}
